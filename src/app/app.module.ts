e7cda3628e13429bced854861e434ad7893414ae
Aleksander Woichansky <awoichansky@speedionet.com>
2019-01-28 07:14:24 +0100

    FSD-47 create main Page With navigation
    - created header
    - created navigation sidebar
    - created main menu with links
    - added some css styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,19 @@ import {AppComponent} from './app.component';
 import {IssuesComponent} from './issues/issues.component';
 
 import {HttpClientModule} from '@angular/common/http';
-import {MatInputModule, MatPaginatorModule, MatTableModule, MatSortModule} from '@angular/material';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatTableModule,
+  MatSortModule,
+  MatSidenavModule
+} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import {HttpErrorHandler} from './http-error-handler.service';
 import {MyMessageService} from './my-message.service';
 import {UnifiedTableComponent} from './unified-table/unified-table.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -25,10 +32,12 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     MatTableModule, MatPaginatorModule, MatSortModule, MatInputModule,
     BrowserAnimationsModule,
     FormsModule, ReactiveFormsModule,
-
+    AppRoutingModule,
+    MatSidenavModule
   ],
   exports: [
-    MatTableModule, FormsModule, ReactiveFormsModule, MatInputModule, MatSortModule],
+    MatTableModule, FormsModule, ReactiveFormsModule, MatInputModule, MatSortModule, MatSidenavModule
+  ],
   providers: [HttpErrorHandler, MyMessageService],
   bootstrap: [AppComponent]
 })
